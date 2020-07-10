de46c703c49f8d02077d56577037cbbdfde61e89
nakamura hajime <h.nakamura0903@gmail.com>
2020-07-10 13:23:51 +0900

    ModPowとModInvを追加した。

---
+++ src/MathUtils.java
@@ -0,0 +1,22 @@
+
+public final class MathUtils {
+    private MathUtils() {}
+
+    public static long modPow(long x, long y, long mod) {
+        long z = 1;
+        while (y > 0) {
+            if (y % 2 == 0) {
+                x = (x * x) % mod;
+                y /= 2;
+            } else {
+                z = (z * x) % mod;
+                y--;
+            }
+        }
+        return z;
+    }
+
+    public static long modInv(long x, long mod) {
+        return modPow(x, mod - 2, mod);
+    }
+}
