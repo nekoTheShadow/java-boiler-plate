f3a48bea620c75157bf0398ca94b4164fdfd8864
nakamura hajime <h.nakamura0903@gmail.com>
2020-07-17 10:13:30 +0900

    Kadane's algorithmを用いて、最大部分配列和を求めるクラスを追加した。

---
+++ src/MaximumSubarray.java
@@ -0,0 +1,52 @@
+/**
+ * Kadane's algorithmを用いて、最大部分配列和を求める。
+ */
+public class MaximumSubarray {
+    public static MaximumSubarray solve(long[] array) {
+        long bestSum = 0;
+        long currentSum = 0;
+
+        int currentStart = 0;
+        int bestStart = -1;
+        int bestEnd = -1;
+
+        for (int currentEnd = 0; currentEnd < array.length; currentEnd++) {
+            if (currentSum < 0) {
+                currentStart = currentEnd;
+                currentSum = array[currentEnd];
+            } else {
+                currentSum += array[currentEnd];
+            }
+
+            if (currentSum > bestSum) {
+                bestSum = currentSum;
+                bestStart = currentStart;
+                bestEnd = currentEnd;
+            }
+        }
+
+        MaximumSubarray maximumSubarray = new MaximumSubarray();
+        maximumSubarray.sum = bestSum;
+        maximumSubarray.start = bestStart;
+        maximumSubarray.end = bestEnd;
+        return maximumSubarray;
+    }
+
+    private MaximumSubarray() {}
+
+    private long sum;
+    private int start;
+    private int end;
+
+    public long getSum() {
+        return sum;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+}
