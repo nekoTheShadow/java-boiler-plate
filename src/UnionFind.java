81a0b881edb0c157189532379e8c5fb876752704
nakamura hajime <h.nakamura0903@gmail.com>
2020-07-28 19:21:14 +0900

    Union Find Treeを追加した

---
+++ src/UnionFind.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class UnionFind {
+    private int[] parent;
+    private int[] size;
+
+    public UnionFind(int n) {
+        this.parent = IntStream.range(0, n).toArray();
+        this.size = new int[n];
+        Arrays.fill(this.size, 0);
+    }
+
+    public int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+        parent[x] = find(x);
+        return parent[x];
+    }
+
+    public boolean same(int x, int y) {
+        return find(x) == find(y);
+    }
+
+    public void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x == y) {
+            return ;
+        }
+
+        if (size[x] < size[y]) {
+            parent[x] = y;
+            size[y] += size[x];
+        } else {
+            parent[y] = x;
+            size[x] += size[y];
+        }
+
+    }
+
+    public int size(int x) {
+        return size[find(x)];
+    }
+}
