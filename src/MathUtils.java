c2e134e10b8aa84403a2895b9a523fb5aae5662d
hajime nakamura <h.nakamura0903@gmail.com>
2020-08-18 22:13:02 +0900

    powを追加

--- src/MathUtils.java
+++ src/MathUtils.java
@@ -19,4 +19,18 @@ public class MathUtils {
     public static long modInv(long x, long mod) {
         return modPow(x, mod - 2, mod);
     }
+
+    public static long pow(long x, long y) {
+        long z = 1;
+        while (y > 0) {
+            if (y % 2 == 0) {
+                x = (x * x);
+                y /= 2;
+            } else {
+                z = (z * x);
+                y--;
+            }
+        }
+        return z;
+    }
 }
