5117cb7eb741dd3fce6d62018aad80dbcb2577c3
nakamura hajime <h.nakamura0903@gmail.com>
2020-10-23 18:51:49 +0900

    ダイクストラ法を実装

---
+++ src/Dijkstra.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class Dijkstra {
+    private int n;
+    private Map<Integer, List<Edge>> edges;
+
+    public Dijkstra(int n) {
+        this.n = n;
+        this.edges = new HashMap<>();
+    }
+
+    public void add(int from, int to, long cost) {
+        this.edges.computeIfAbsent(from, unused -> new ArrayList<>()).add(new Edge(to, cost));
+    }
+
+    public long[] run(int start) {
+        long[] score = new long[n];
+        Arrays.fill(score, Long.MAX_VALUE);
+        score[start] = 0;
+
+        PriorityQueue<Tuple> q = new PriorityQueue<>();
+        q.add(new Tuple(start, 0));
+        while (!q.isEmpty()) {
+            Tuple t = q.remove();
+            for (Edge e : edges.getOrDefault(t.current, Collections.emptyList())) {
+                if (t.cost + e.cost < score[e.next]) {
+                    score[e.next] = t.cost + e.cost;
+                    q.add(new Tuple(e.next, score[e.next]));
+                }
+            }
+        }
+        return score;
+    }
+
+    private class Edge {
+        private int next;
+        private long cost;
+
+        public Edge(int next, long cost) {
+            this.next = next;
+            this.cost = cost;
+        }
+    }
+
+    private class Tuple implements Comparable<Tuple>{
+        private int current;
+        private long cost;
+
+        public Tuple(int current, long cost) {
+            this.current = current;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Tuple other) {
+            return Long.compare(this.cost, other.cost);
+        }
+    }
+}
