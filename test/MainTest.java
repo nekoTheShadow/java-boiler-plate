95395dd1c75c0bd295534cf34c60529e5e7b710c
hajime nakamura <h.nakamura0903@gmail.com>
2020-10-25 21:18:08 +0900

    マイナス値のテスト

--- test/MainTest.java
+++ test/MainTest.java
@@ -9,7 +9,7 @@ public class MainTest {
 
     @Test
     public void testStdin() {
-        InputStream is = new ByteArrayInputStream("111 222 333\nABC DEF GHI\r\n444 555\n66.66".getBytes());
+        InputStream is = new ByteArrayInputStream("111 222 333\nABC DEF GHI\r\n444 555\n66.66\r\n-77 -88".getBytes());
         Main.Stdin stdin = new Main.Stdin(is);
         assertEquals(111, stdin.nextInt());
         assertEquals(222, stdin.nextInt());
@@ -20,5 +20,7 @@ public class MainTest {
         assertEquals(444, stdin.nextLong());
         assertEquals(555, stdin.nextLong());
         assertEquals(66.66, stdin.nextDouble());
+        assertEquals(-77, stdin.nextInt());
+        assertEquals(-88, stdin.nextInt());
     }
 }
