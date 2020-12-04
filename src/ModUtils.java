8f9f6aa99ca94a80792d59590cf9feec0c3e7287
hajime nakamura <h.nakamura0903@gmail.com>
2020-12-04 12:18:02 +0900

    modutilを全面改修

--- src/ModUtils.java
+++ src/ModUtils.java
@@ -1,19 +1,11 @@
+import java.math.BigInteger;
+
 public class ModUtils {
     public long modPow(long x, long y, long mod) {
-        long z = 1;
-        while (y > 0) {
-            if (y % 2 == 0) {
-                x = (x * x) % mod;
-                y /= 2;
-            } else {
-                z = (z * x) % mod;
-                y--;
-            }
-        }
-        return z;
+        return BigInteger.valueOf(x).modPow(BigInteger.valueOf(y), BigInteger.valueOf(mod)).longValue();
     }
 
     public long modInv(long x, long mod) {
-        return modPow(x, mod - 2, mod);
+        return BigInteger.valueOf(x).modInverse(BigInteger.valueOf(mod)).longValue();
     }
 }
