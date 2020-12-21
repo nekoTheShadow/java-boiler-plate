a9445c293b91450405447e7e594b62a4326d978f
nakamura hajime <h.nakamura0903@gmail.com>
2020-12-21 19:09:53 +0900

    safeModを追加した。

--- src/ModUtils.java
+++ src/ModUtils.java
@@ -8,4 +8,13 @@ public class ModUtils {
     public long modInv(long x, long mod) {
         return BigInteger.valueOf(x).modInverse(BigInteger.valueOf(mod)).longValue();
     }
+
+    public long safeMod(long x, long y) {
+        long mod = x % y;
+        if (mod >= 0) {
+            return mod;
+        } else {
+            return (mod+y)%y;
+        }
+    }
 }
