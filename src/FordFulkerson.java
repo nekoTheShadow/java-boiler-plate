4973d6a40990d74a35e249ddc786bb8b12bb028b
hajime nakamura <h.nakamura0903@gmail.com>
2021-02-02 00:54:40 +0900

    最大フロー・最小カットを追加

---
+++ src/FordFulkerson.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FordFulkerson {
+    private class Edge {
+        private int from;
+        private int to;
+        private long cost;
+        private int rev;
+        public Edge(int from, int to, long cost, int rev) {
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
+            this.rev = rev;
+        }
+    }
+
+    private List<List<Edge>> g;
+    private boolean[] seen;
+
+    public FordFulkerson(int n) {
+        this.g = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            this.g.add(new ArrayList<>());
+        }
+        this.seen = new boolean[n];
+    }
+
+    public void addEdge(int from, int to, long cost) {
+        int fromRev = this.g.get(from).size();
+        int toRev = this.g.get(to).size();
+        this.g.get(from).add(new Edge(from, to, cost, toRev));
+        this.g.get(to).add(new Edge(to, from, 0, fromRev));
+    }
+
+    private void runFlow(Edge e, long f) {
+        e.cost -= f;
+        this.g.get(e.to).get(e.rev).cost += f;
+    }
+
+    private long dfs(int v, int t, long f) {
+        if (v==t) return f;
+
+        this.seen[v] = true;
+        for (Edge e : this.g.get(v)) {
+            if (this.seen[e.to]) continue;
+            if (e.cost == 0) continue;
+
+            long flow = this.dfs(e.to, t, Math.min(f, e.cost));
+            if (flow==0) continue;
+
+            this.runFlow(e, flow);
+
+            return flow;
+        }
+
+        return 0;
+    }
+
+    public long solve(int s, int t) {
+        long res = 0;
+        while (true) {
+            Arrays.fill(this.seen, false);
+            long flow = this.dfs(s, t, Long.MAX_VALUE);
+            if (flow==0) break;
+            res += flow;
+        }
+        return res;
+    }
+}
