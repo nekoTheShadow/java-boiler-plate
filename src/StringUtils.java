3fd2b019cb2f6afac795d8035e9422a59662c88f
hajime nakamura <h.nakamura0903@gmail.com>
2021-10-25 19:18:08 +0900

    Manacher's algorithmを実装

--- src/StringUtils.java
+++ src/StringUtils.java
@@ -1,4 +1,3 @@
-
 public class StringUtils {
     public String lcs(String s, String t) {
         int n = s.length();
@@ -31,4 +30,64 @@ public class StringUtils {
 
         return u.reverse().toString();
     }
+    
+    /**
+     * 最長の回文を探索する。計算量はO(N)。
+     * https://en.wikipedia.org/wiki/Longest_palindromic_substring
+     */
+    public String longestPalindrome(String s, char dummy) {
+        char[] t = new char[s.length()*2+1];
+        for (int i = 0; i < s.length(); i++) {
+            t[i*2] = dummy;
+            t[i*2+1] = s.charAt(i);
+        }
+        t[t.length-1] = dummy;
+        int[] palindromeRadii = new int[t.length];
+        
+        int center = 0;
+        int radius = 0;
+        while (center < t.length) {
+            while (center-(radius+1) >= 0 && center+(radius+1) < t.length && t[center-(radius+1)] == t[center+(radius+1)]) {
+                radius++;
+            } 
+            
+            palindromeRadii[center] = radius;
+            
+            int oldCenter = center;
+            int oldRadius = radius;
+            center++;
+            radius = 0;
+            while (center <= oldCenter + oldRadius) {
+                int mirroredCenter = oldCenter - (center - oldCenter);
+                int maxMirroredRadius = oldCenter + oldRadius - center;
+                if (palindromeRadii[mirroredCenter] < maxMirroredRadius) {
+                    palindromeRadii[center] = palindromeRadii[mirroredCenter];
+                    center++;
+                } else if (palindromeRadii[mirroredCenter] > maxMirroredRadius) {
+                    palindromeRadii[center] = maxMirroredRadius;
+                    center++;
+                } else {
+                    radius = maxMirroredRadius;
+                    break;
+                }
+            }
+        }
+        
+        int pivot = 0;
+        for (int i = 1; i < palindromeRadii.length; i++) {
+            if (palindromeRadii[pivot] < palindromeRadii[i]) {
+                pivot = i;
+            }
+        }
+        
+        StringBuilder ans = new StringBuilder();
+        for (int i = pivot-palindromeRadii[pivot]+1; i < pivot+palindromeRadii[pivot]; i++) {
+            if (t[i] != dummy) {
+                ans.append(t[i]);
+            }
+        }
+        
+        return ans.toString();
+    }
 }
+
