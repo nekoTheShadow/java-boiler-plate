a2fe06d65960ed019640ac4f6181dd89da3272ae
neko_the_shadow <h.nakamura0903@gmail.com>
2022-01-30 11:25:37 +0900

    2次元いもす法を再実装

--- src/Imos2.java
+++ src/Imos2.java
@@ -1,30 +1,36 @@
-
+    
 public class Imos2 {
+    private int h;
+    private int w;
     private long[][] imos;
     
     public Imos2(int h, int w) {
+        this.h = h;
+        this.w = w;
         this.imos = new long[h+1][w+1];
     }
     
+    // 矩形の左上(lx, ly)、矩形の右下(rx, ry)
     public void add(int lx, int ly, int rx, int ry) {
         imos[lx][ly]++;
-        imos[lx][ry]--;
-        imos[rx][ly]--;
-        imos[rx][ry]++;
+        imos[lx][ry+1]--;
+        imos[rx+1][ly]--;
+        imos[rx+1][ry+1]++;
     }
     
     public long[][] run() {
-        for (int y = 0; y <= 1000; y++) {
-            for (int x = 1; x <= 1000; x++) {
-                imos[y][x] += imos[y][x - 1];
+        for (int i = 0; i <= h; i++) {
+            for (int j = 1; j <= w; j++) {
+                imos[i][j] += imos[i][j-1];
             }
         }
         
-        for (int y = 1; y <= 1000; y++) {
-            for (int x = 0; x <= 1000; x++) {
-                imos[y][x] += imos[y - 1][x];
+        for (int i = 1; i <= h; i++) {
+            for (int j = 0; j <= w; j++) {
+                imos[i][j] += imos[i-1][j];
             }
         }
+        
         return imos;
     }
 }
