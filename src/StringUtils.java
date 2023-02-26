39da1ae8bf74f5684ee682d9f1900b35cdccbe84
neko_the_shadow <h.nakamura0903@gmail.com>
2023-02-26 09:40:01 +0900

    レーベンシュタイン距離/編集距離を追加

--- src/StringUtils.java
+++ src/StringUtils.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class StringUtils {
     public String lcs(String s, String t) {
@@ -138,6 +139,26 @@ public class StringUtils {
             }
         }
         return ans;
+    }
+    
+    public int levenshteinDistance(String word1, String word2) {
+        int n1 = word1.length();
+        int n2 = word2.length();
+        int[][] dp = new int[n1+1][n2+1];
+        for (int i = 0; i <= n1; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= n2; j++) {
+            dp[0][j] = j;
+        }
+        
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                int c = word1.charAt(i-1)==word2.charAt(j-1) ? 0 : 1;
+                dp[i][j] = IntStream.of(dp[i-1][j]+1, dp[i][j-1]+1, dp[i-1][j-1]+c).min().getAsInt();
+            }
+        }
+        return dp[n1][n2];
     }
 }
 
