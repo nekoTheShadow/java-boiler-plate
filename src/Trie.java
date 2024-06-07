7af5bdbf8d59f56329bc162a1dfca74a1c2f69ba
neko_the_shadow <h.nakamura0903@gmail.com>
2024-06-07 12:16:01 +0900

    Trie木を追加

--- src/Trie.java
+++ src/Trie.java
@@ -1,59 +1,50 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-// トライ木(Trie木) https://algo-logic.info/trie-tree/
-public class Trie {
-    private Node root;
-    
-    public Trie() {
-        this.root = new Node('\0');
-    }
-    
-    // 文字列の挿入
-    public void insert(String word) {
-        Node cur = root;
-        for (char c : word.toCharArray()) {
-            if (cur.nexts.containsKey(c)) {
-                cur = cur.nexts.get(c);
-            } else {
-                Node next = new Node(c);
-                cur.nexts.put(c, next);
-                cur = next;
-            }
-        }
-        cur.accept = true;
-    }
-    
-    // 文字列の検索 (完全一致)
-    public boolean search(String word) {
-        return search(word, false);
-    }
-    
-    // 文字列の検索 (前方一致)
-    public boolean startsWith(String prefix) {
-        return search(prefix, true);
-    }
-    
-    private boolean search(String word, boolean prefix) {
-        Node cur = this.root;
-        for (char c : word.toCharArray()) {
-            if (!cur.nexts.containsKey(c)) {
-                return false;
-            }
-            cur = cur.nexts.get(c);
-        }
-        return prefix || cur.accept;
-    }
-    
-    public static class Node {
-        public char c;
-        public boolean accept;
-        public Map<Character, Node> nexts;
-        
-        public Node(char c) {
-            this.c = c;
-            this.accept = false;
-            this.nexts = new HashMap<>();
-        }
-    }
+public class Trie<T> {
+	private Node<T> root;
+	
+	public Trie() {
+		this.root = new Node<T>();
+	}
+	
+	public void insert(String key, T value) {
+		Node<T> node = root;
+		for (char ch : key.toCharArray()) {
+			if (!node.children.containsKey(ch)) {
+				node.children.put(ch, new Node<T>());
+			}
+			node = node.children.get(ch);
+		}
+		node.value = value;
+	}
+	
+	// trieに登録されている文字をsとする
+	// key.start_with(s)を満たす、すべてのvalueを返す
+	public List<T> query(String key) {
+		List<T> values = new ArrayList<>();
+		Node<T> node = root;
+		for (char ch : key.toCharArray()) {
+			if (node.value != null) {
+				values.add(node.value);
+			}
+			if (!node.children.containsKey(ch)) {
+				break;
+			}
+			node = node.children.get(ch);
+		}
+		return values;
+	}
+	
+	private static class Node<T> {
+		private Map<Character, Node<T>> children;
+		private T value;
+		
+		public Node() {
+			this.children = new HashMap<>();
+			this.value = null;
+		}
+	}
 }
